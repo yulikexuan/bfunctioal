21a127504bb4c81989b5ae0963662cc41979eca7
yulikexuan <yulikexuan@gmail.com>
2017-11-07 09:18:50 -0500

    Initial commit

---
+++ src/main/java/com/yuli/bfunctional/BfunctionalApplication.java
@@ -0,0 +1,12 @@
+package com.yuli.bfunctional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfunctionalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yuli/bfunctional/BfunctionalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuli.bfunctional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BfunctionalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
