a348639de61bd1995a5b2e7dea3b6bd36ef3fabc
yulikexuan <yulikexuan@gmail.com>
2018-06-01 17:36:37 -0400

    New test for Shop

---
+++ src/test/java/com/yuli/bfunctional/j8ia/domain/model/async/ShopTest.java
@@ -0,0 +1,19 @@
+//: com.yuli.bfunctional.j8ia.domain.model.async.ShopTest.java
+
+
+package com.yuli.bfunctional.j8ia.domain.model.async;
+
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+public class ShopTest {
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+}///:~
